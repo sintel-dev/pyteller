5a9a46030fc80743a626413f2dc101063307cf2b
Skylar Eiskowitz <skyeeiskowitz@gmail.com>
2020-10-12 19:13:02 -0400

    git ignore

--- setup.py
+++ setup.py
@@ -16,6 +16,17 @@ install_requires = [
 
 setup_requires = [
     'pytest-runner>=2.11.1',
+    's3fs>=0.2.2,<0.5',
+    'baytune>=0.2.3,<0.3',
+    'mlblocks>=0.3.0,<0.4',
+    'mlprimitives>=0.2.2,<0.3',
+    'mongoengine>=0.16.3,<0.17',
+    'numpy>=1.15.4,<1.17',
+    'pandas>=0.23.4,<0.25',
+    'pymongo>=3.7.2,<4',
+    'scikit-learn>=0.20.1,<0.21',
+    'tabulate>=0.8.3,<0.9',
+    'Keras>=2.1.6,<2.4'
 ]
 
 tests_require = [
