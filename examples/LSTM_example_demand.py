2be8ca7481b90c0ba3e2f06756b32f9d3b11901d
Skylar Eiskowitz <skyeeiskowitz@gmail.com>
2020-11-25 23:31:36 -0500

    Add data

--- examples/LSTM_example_demand.py
+++ examples/LSTM_example_demand.py
@@ -3,14 +3,15 @@ from pyteller.data import load_data
 # Load the dataset from a dataset on the s3 bucket, or in this example the local file path
 from pyteller.data import load_data
 # current_data=load_data('../pyteller/data/Demand_average_train.csv').iloc[0:6000,:]
-current_data=load_data('../pyteller/data/Demand_max_train.csv')#.iloc[0:6000,:]
+current_data=load_data('../pyteller/data/taxi.csv')#.iloc[0:6000,:]
+# current_data=load_data('../pyteller/data/Demand_max_train.csv')#.iloc[0:6000,:]
 
 
 # Initialization settings: user doesn't change these once new data comes in
 from pyteller.core import Pyteller
 pyteller = Pyteller (
     # pipeline='lagged_demand_average',
-    pipeline='lstm_demand_max',
+    pipeline='../pyteller/pipelines/sandbox/LSTM/lstm_demand_max.json',
     # pred_length=10,
     # offset=5,
     goal=None,
@@ -19,14 +20,15 @@ pyteller = Pyteller (
 
 pyteller.fit(
     data=current_data,
-    timestamp_col='DateTime',
-    target_signals='T1_FWD',
+    timestamp_col='timestamp',
+    target_signals='value',
     static_variables=None,
     # entity_col='station',
     train_size=.75)
 
-input_data=load_data('../pyteller/data/Demand_average_test.csv')
-input_data=load_data('../pyteller/data/Demand_max_test.csv')
+input_data=load_data('../pyteller/data/taxi_test.csv')
+# input_data=load_data('../pyteller/data/Demand_average_test.csv')
+# input_data=load_data('../pyteller/data/Demand_max_test.csv')
 y,y_hat = pyteller.forecast(data=input_data)
 
 
