8dee87150f8d642fc101109216c1749ea8a1e284
Skylar Eiskowitz <skyeeiskowitz@gmail.com>
2021-01-15 18:34:39 -0500

    set up logger

--- pyteller/utils.py
+++ pyteller/utils.py
@@ -60,3 +60,26 @@ def plot(dfs, output_path, labels=None):
     os.path.join('figs', output_path)
     plt.savefig('figs/lstm.png')
     plt.show()
+
+import logging
+
+
+def logging_setup(verbosity=1, logfile=None, logger_name=None):
+    logger = logging.getLogger(logger_name)
+    log_level = (3 - verbosity) * 10
+    fmt = '%(asctime)s - %(process)d - %(levelname)s - %(module)s - %(message)s'
+    formatter = logging.Formatter(fmt)
+    logger.setLevel(log_level)
+    logger.propagate = False
+
+    if logfile:
+        file_handler = logging.FileHandler(logfile)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    else:
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(log_level)
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
