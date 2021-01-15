d260e214155a66a0fc7998e283f7757e200e16a6
Skylar Eiskowitz <skyeeiskowitz@gmail.com>
2021-01-15 15:46:25 -0500

    move ingest egest inside pyteller class

--- pyteller/__init__.py
+++ pyteller/__init__.py
@@ -9,10 +9,10 @@ __version__ = '0.1.1.dev0'
 import os
 
 _BASE_PATH = os.path.abspath(os.path.dirname(__file__))
-MLBLOCKS_PRIMITIVES = [os.path.join(_BASE_PATH, 'primitives', 'jsons'),
-                       os.path.join(_BASE_PATH, '../', 'pyteller', 'primitives', 'jsons')]
-# MLBLOCKS_PIPELINES = os.path.join(_BASE_PATH, 'pipelines')
-MLBLOCKS_PIPELINES = tuple(
-    dirname
-    for dirname, _, _ in os.walk(os.path.join(_BASE_PATH, 'pipelines'))
-)
+MLBLOCKS_PIPELINES = os.path.join(_BASE_PATH, 'pipelines')
+MLBLOCKS_PRIMITIVES = [os.path.join(_BASE_PATH, 'primitives', 'jsons')]
+
+
+
+
+
