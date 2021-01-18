a2009a090e987d3f5d4fbd87f25a2447628d76c5
Skylar Eiskowitz <skyeeiskowitz@gmail.com>
2021-01-18 14:53:44 -0500

    comment

--- examples/Persistence_example.py
+++ examples/Persistence_example.py
@@ -1,11 +1,13 @@
-
 # Load the dataset from a dataset on the s3 bucket, or in this example the local file path
 from pyteller.data import load_data
 current_data = load_data('pyteller/data/AL_Weather_current.csv')
 
-from pyteller.core import Pyteller, ingest_data
-
+# Specify pipeline and hyperparamters if any
 pipeline = 'pyteller/pipelines/sandbox/persistence/persistence.json'
+
+# Make instance of Pyteller, specifying where to make the prediction and the column names
+
+from pyteller.core import Pyteller, ingest_data
 pyteller = Pyteller(
     pipeline=pipeline,
     pred_length=5,
@@ -16,14 +18,21 @@ pyteller = Pyteller(
     entity_col='station',
     # entities='8A0',
 )
+
+# Ingest the data if you wish to be able to see the format that will go into the pipeline
 current_data = ingest_data(pyteller, current_data)
+
+# Fit the data to the pipeline
 pyteller.fit(current_data, ingested_data = True)
 
+# Load and ingest the input_data
 input_data=load_data('pyteller/data/AL_Weather_input.csv')
 input_data = ingest_data(pyteller, input_data)
-y,y_hat = pyteller.forecast(data=input_data, ingested_data = True)
 
+# forecast and evaluate
+y,y_hat = pyteller.forecast(data=input_data, ingested_data = True)
 scores= pyteller.evaluate(forecast=y_hat, test_data=y, metrics=['MAPE','sMAPE'])
 
+# plot
 from pyteller.utils import plot
 plot([y.iloc[:,0:1],y_hat.iloc[:,0:1]],frequency='day')
